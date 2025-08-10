cd24ca67879c60cf984cdb82319a0c2641019f69
listmen001 <za.ignatev@gmail.com>
2025-08-10 21:26:14 +0400

    ii3

---
+++ ii3.py
@@ -0,0 +1,15 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+# Запрашиваем диапазон
+start = int(input("Введите начало диапазона: "))
+end = int(input("Введите конец диапазона: "))
+
+# Находим простые числа
+primes = [num for num in range(start, end + 1) if is_prime(num)]
+print("Простые числа:", primes)
